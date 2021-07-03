5bcf5966c92e23a666b09b08e0b9edeecbd778da
Nadim Mouawad <nadimetto@gmail.com>
2021-07-03 18:15:16 +0300

    added again https and tried to make icons dynamic

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-fetch('http://vlava-api-nadim.herokuapp.com/services')
+fetch('https://vlava-api-nadim.herokuapp.com/services')
   .then(response => response.json())
   .then(json => 
     {
     console.log(json)
     var allServices = document.getElementById('AllServices');
     var content =   `<div>
-                    <div class="icon"><i class="bi bi-laptop"></i></div>
+                    <div class="icon"><i class="ICONS"></i></div>
                     <h4 class="title"><a href="">Lorem Ipsum</a></h4>
                     <p class="description">Voluptatum</p>
                     </div>`
@@ -14,6 +14,7 @@ fetch('http://vlava-api-nadim.herokuapp.com/services')
     {     
         var specificContent = content.replace("Lorem Ipsum",json[i].title)  
         specificContent = specificContent.replace("Voluptatum",json[i].description)
+        specificContent = specificContent.replace("ICONS", json[i].image)
         var singleService = document.createElement('div')        
         singleService.className="col-lg-4 col-md-6 icon-box";            
         singleService.innerHTML = specificContent;
